e8d927d69a9ffd8e2ead7c601ffea473dccae687
Ned Reifenstein <nreifenstein@berkeley.edu>
2013-06-11 17:46:01 -0700

    update

--- Empty.py
+++ Empty.py
@@ -1,14 +1,126 @@
-import decodes
-
+from decodes.core import *
+from decodes.core import dc_color, dc_base, dc_vec, dc_point, dc_cs, dc_line, dc_mesh, dc_pgon, dc_xform
 import thesis
-from thesis import ants
+from thesis.ants.ants import Graph
+
+from decodes.extensions.cellular_automata import CA
+import random
 
 
+prop_colors = (Color(0.0),Color(0,1,0),Color(0,.5,.5))
+state_list = ('undeveloped','green','built')
+no_states = len(state_list)
+no_gen = 10
 
 
+import os
+path = os.path.expanduser("~") + os.sep + "_ants_export"
+f_prefix = "sobe_"
+
+sc = 1
+p = [1]
+m = 100
+n = 100
+w = False
+
+def a_count(v,a):
+    ret = 0
+    for i in a: 
+        if i == v : ret += 1
+    return ret
+    
+
+## this component will create a graph assuming an m x n grid of cells with a cell size of sc
+## 05.22.13 added cell into data type
+
+DELETE = -1
+UNDEVELOPED = 0
+ACCESS = 1
+BUILT = 11
 
 
+WEST = 0
+NORTH = 1
+EAST = 2
+SOUTH =3
+dirs =["WEST","NORTH","EAST","SOUTH"]
+v = [[-1,0,0],[0,1,0],[1,0,0],[0,-1,0]]                                 # contains vector displacements for cardinal directions WNES
+scales = [[.5,1],[1,.5],[.5,1],[1,.5]]
+deltas = [[.5,0],[0,-.5],[-.5,0],[0,.5]]
+
+MOORE = 0                                   ## 8-cell neighborhood
+VON_NEUMANN = 1                             ## 4-cell neighborhood
+NEIGHBORHOOD_TYPE = VON_NEUMANN
+
+EPSILON = sc * .001                  ## tolerance for touch
 
 
+m_domain = list(range(m))
+n_domain = list(range(n))
+cell_half = sc/2
+
+# initialize lists
+pts = []
+neighbors = []
+cells = []
+
+# create neighbor list for standard rectangular cell grid
+for j in n_domain:
+    for i in m_domain:
+        pts.append([cell_half + sc*i,cell_half+sc*j,0])
+        cells.append([sc,sc,1])
+        k = i+j*m
+        n_new = []
+        for di in [-1,0,1]:
+            for dj in [-1,0,1]:
+                if (abs(di)+abs(dj)) > 0:
+                    if w :          # wrap is true
+                        new_index = n_domain[(j+dj)%n]*m+m_domain[(i+di)%m]
+                        if (di == 0) or (dj == 0): n_new.append(new_index)
+                        elif NEIGHBORHOOD_TYPE == MOORE : n_new.append(new_index)
+                    else:           # wrap is false
+                        if ((i+di) in m_domain) and ((j+dj) in n_domain):
+                            if (di == 0) or (dj == 0) : n_new.append((j+dj)*m+(i+di))
+                            elif NEIGHBORHOOD_TYPE == MOORE : n_new.append((j+dj)*m+(i+di))
+        neighbors.append(n_new)
+
+
+props = []
+for i in range(m*n): 
+    if random.uniform(0.0,1.0) < .01 :
+        props.append(1)
+    else:
+        props.append(0)
+#    props.append(random.choice(range(no_states)))
+
+g_temp = []
+g_temp = Graph(neighbors,pts,cells,props)
+
+gen = 0
+h=[]
+h.append(g_temp)
+
+# history loop
+while gen < no_gen:
+    img = h[gen].to_image(Interval(m,n),prop_colors)
+    img.save(f_prefix+str(gen), path, True)
+    new_props = (m*n)*[0]
+    for i in range(m*n):
+        if h[gen].prop[i] == 0:
+            n_temp = h[gen].n_props(i)
+            c_temp = a_count(1,n_temp)
+            if c_temp == 1:
+                new_props[i] = random.choice([1,2])
+            elif c_temp == 4:
+                new_props[i] = 1
+            elif c_temp == 2:
+                new_props[i] = 1
+            elif c_temp == 3:
+                new_props[i] = 2
+            else:
+                new_props[i] = 0
+        else: new_props[i] = h[gen].prop[i]
+    h.append(Graph(neighbors,pts,cells,new_props))
+    gen+=1
 
 raw_input("press enter...")
--- thesis/ants/ants.py
+++ thesis/ants/ants.py
@@ -1,64 +1,224 @@
-from ..core import *
-from ..core import dc_color, dc_base, dc_vec, dc_point, dc_cs, dc_line, dc_mesh, dc_pgon, dc_xform
+from decodes.core import *
+from decodes.core import dc_color, dc_base, dc_vec, dc_point, dc_cs, dc_line, dc_mesh, dc_pgon, dc_xform
 import copy
-print "cellular_automata.py loaded"
+print "ants.py loaded"
 
-class CA (object):
 
-    def __init__(self,pixel_res=Interval(20,20),include_corners=False,wrap=False):
-        self.width = pixel_res.a
-        self.height = pixel_res.b
-        self.include_corners = include_corners
-        self.wrap = wrap
-        self.clear()
+
+## define array utility functions
+
+def a_add_like(a1,a2):
+    result = []
+    for i in range(min(len(a1),len(a2))):
+        result.append(a1[i]+a2[i])
+    return result
+
+def a_sub_like(a1,a2):
+    result = []
+    for i in range(min(len(a1),len(a2))):
+        result.append(a1[i]-a2[i])
+    return result
+
+def a_mult_like(a1,a2):
+    result = []
+    for i in range(min(len(a1),len(a2))):
+        result.append(a1[i]*a2[i])
+    return result
+
+def a_scalar(s,a1):
+    result = []
+    for i in range(len(a1)):
+        result.append(s*a1[i])
+    return result
     
-    def set_u(self,x,y,val):
-        x,y = self._reframe(x,y)
-        self._uvals.set(x,y,val)
+def a_rem_dup(a1):
+    result = []
+    for i in range(len(a1)):
+        dup = False
+        for j in range(len(result)):
+            if a1[i] == result[j] : dup = True
+        if not dup : result.append(a1[i])
+    return result
 
-# pass a function to be used for each iteration of the automata
-# assumes that function takes the status of the home cell, and a list of the statuses of the neighbors
-# function returns the new value for the home cell
-    def set_rule(self,func=False):
-        self.rule = func
+def a_count(v,a):
+    ret = 0
+    for i in a: 
+        if i == v : ret += 1
+    return ret
+    
+## define graph class
 
-    def get_u(self,x,y): return self._uvals.get(x,y)
-
-    def _reframe(self,x,y):
-        while x > self.width -1 : x = x - self.width
-        while y > self.height -1: y = y - self.height
-        while x < 0 : x = self.width + x
-        while y < 0 : y = self.height + y
-        return x,y
-
-    def clear(self):
-        self._uvals = BoolField(Interval(self.width,self.height),False,self.include_corners)
-        self.step_count = 0
-        self.hist_u = []
-
-    def record(self):
-        self.hist_u.append(self._uvals)
-
-# makes a new generation by calling the function stored in self.rule
-
-    def step(self, t=1.0):
-        nxt_uvals = BoolField(Interval(self.width,self.height),False,self.include_corners)
-        t = max(min(1.0,t),0.0)
-        for x in range(0,self.width):
-            for y in range(0,self.height):
-                cur_u = self._uvals.get(x,y)
-                neighbors_u = self._uvals.neighbors_of(x,y)
-                nxt_u = self.rule(cur_u, neighbors_u)
-                nxt_uvals.set(x,y,nxt_u)
- #               self.log_u(nxt_u)
- #       self.hist_u.append(self._uvals)
-        self._uvals = nxt_uvals
-        self.step_count += 1
-
-# sets the initial condition for the CA. Expects a list of booleans
-    def start(self, initial_uvals=False):
-        for n, bool in enumerate(initial_uvals):
-            self._uvals._pixels[n] = bool
-
-
+class Graph():
+    def __init__(self,_list,_pts,_cells,_props):
+        self.links = _list
+        self.pts = _pts
+        self.cell = _cells
+        self.prop = _props
+        self._res = len(_props)
         
+    def addcell(self, _links, _pt, _cell,_prop):
+        self.links.append(_links)
+        self.pts.append(_pt)
+        self.cell.append(_cell)
+        self.prop.append(_prop)
+        return len(self.pts)-1
+        
+    def update(self):
+        for i in range(len(self.links)): self.links[i] = []
+        for i in range(len(self.links)-1):
+            for j in range(i+1,len(self.links)):
+                if touch(self.pts[i],self.cell[i],self.pts[j],self.cell[j]):
+                    self.links[i].append(j)
+                    self.links[j].append(i)
+                    
+    def direction(self,i,j):
+        p1 = self.pts[i]
+        p2 = self.pts[j]
+        dx = p1[0]-p2[0]
+        dy = p1[1]-p2[1]
+        if abs(dx) > abs(dy):
+            if dx > 0 : return EAST
+            else: return WEST
+        else:
+            if dy > 0 : return NORTH
+            return SOUTH
+            
+            
+    def neighbor(self, i,j):
+        if i == j : return False                   
+        p1 = self.pts[i]
+        p2 = self.pts[j]
+        c1 = self.cell[i]
+        c2 = self.cell[j]
+        #print "p1 ",p1," p2 ",p2," c1 ",c1," c2 ",c2
+        dx = abs(p1[0] - p2[0])             ## get absolute value of distances between basepoints
+        dy = abs(p1[1] - p2[1])
+        dz = abs(p1[2] - p2[2])
+        lx = c1[0]/2 + c2[0]/2     ## get sum of widths, height, lengths
+        ly = c1[1]/2 + c2[1]/2 
+        lz = 0
+        if NEIGHBORHOOD_TYPE == MOORE:
+           return (dx <= lx + EPSILON) and (dy <= ly + EPSILON) and (dz <= lz+ EPSILON)
+        elif NEIGHBORHOOD_TYPE == VON_NEUMANN:
+            if abs(dx - lx) < EPSILON:
+                return dy < ly - EPSILON
+            if abs(dy - ly) < EPSILON:
+                return dx < lx - EPSILON
+            return False
+        else: return False
+            
+    def divide(self,i,d,q):     ## i = cell index, d = direction, q = fraction
+        s1 = [[1-q,1,1],[1,1-q,1],[1-q,1,1],[1,1-q,1]]                  # contains scale factors for new cell
+        s2 = [[q,1,1],[1,q,1],[q,1,1],[1,q,1]]          # contains scale factors for remainder
+
+        c1 = a_mult_like(self.cell[i],s1[d])                    # creates new cell sizes
+        c2 = a_mult_like(self.cell[i],s2[d])
+
+        b1 = a_add_like(self.pts[i],a_scalar(.5,a_mult_like(c2,v[(d+2)%4])))            # creates new base points
+        b2 = a_add_like(self.pts[i],a_scalar(.5,a_mult_like(c1,v[d])))
+
+        new_i = self.addcell([i],b2,c2,self.prop[i])                                                      # make new cells
+        
+        n = self.links[i]                   # get neighbors
+        self.links[i] = [new_i]
+        self.pts[i] = b1
+        self.cell[i] = c1
+        for j in n:
+            if i in self.links[j] : self.links[j].remove(i)
+            for k in [i,new_i]:
+                if self.neighbor(j,k):
+                    self.links[j].append(k)
+                    self.links[k].append(j)
+        return True
+        
+        
+    def combine(self,i,j):
+        print "testing ",i,j
+        if not(self.neighbor(i,j)) : return False          # are not adjacent
+        test = False
+        for k in [0,1]:                                 # check if they are same h/w
+            #print self.pts[i][k],self.pts[j][k]
+            if (abs(self.pts[i][k]-self.pts[j][k]) < EPSILON) and (self.cell[i][k] == self.cell[j][k]): test = True
+        if not(test) : return False                     # not same h/w
+        print i,j," can be combined"
+        n_new = a_rem_dup(self.links[i]+self.links[j])
+        if i in n_new : n_new.remove(i)
+        if j in n_new : n_new.remove(j)
+        d = direction(self.pts[j],self.pts[i])
+        if d%2 == 0: self.cell[i][0]+=self.cell[j][0]
+        else: self.cell[i][1]+=self.cell[j][1]
+        self.pts[i] = a_add_like(self.pts[i],a_scalar(.5,a_mult_like(self.cell[j],v[d])))
+        self.links[j] = []
+        self.links[i] = n_new
+        self.prop[j]=DELETE
+        print "for cell ",j," :",self.links[j], self.prop[j]
+        for k in n_new:
+            if j in self.links[k] : self.links[k].remove(j)
+        return True
+        
+    def find(self,prop):
+        result = []
+        for i in list(range(len(self.prop))):
+            if self.prop[i] == prop : result.append(i)
+        return result
+        
+    def n_props(self,c):
+        result = []
+        for i in self.links[c]:
+            result.append(self.prop[i])
+        return result
+        
+    def have_neighbor(self,mp,np):              # create list of cells with (1) props in mp and (2) neighbors with np
+        result = []
+        for i in range(len(self.prop)):
+            if mp == self.prop[i]:
+                if np in self.n_props(i):
+                    result.append(i)
+        return result
+
+    def to_image(self,pixel_res = Interval(20,20),color_list = [Color(0.0),Color(1.0)]):
+        img = Image(pixel_res,color_list[0])
+        for n, val in enumerate(self.prop):
+            img._pixels[n] = color_list[val]
+        return img
+
+
+
+
+
+def p_index(test, list_of_points):
+    ret_value = -1
+    for i in list(range(len(list_of_points))):
+        if Segment(test, list_of_points[i]).length < .1 :
+            return i
+    return -1
+
+def direction(p1,p2):
+    dx = p1[0]-p2[0]
+    dy = p1[1]-p2[1]
+    if abs(dx) > abs(dy):
+        if dx > 0 : return EAST
+        else: return WEST
+    else:
+        if dy > 0 : return NORTH
+        return SOUTH
+
+def touch(p1,c1,p2,c2):
+    dx = abs(p1[0] - p2[0])             ## get absolute value of distances between basepoints
+    dy = abs(p1[1] - p2[1])
+    dz = abs(p1[2] - p2[2])
+    #print "in touch. dx,dy,dz: ", dx, dy, dz
+    lx = c1[0]/2 + c2[0]/2     ## get sum of widths, height, lengths
+    ly = c1[1]/2 + c2[1]/2 
+    lz = 0
+    #print "in touch. lx,ly,lz: ", lx,ly,lz
+    if NEIGHBORHOOD_TYPE == MOORE:
+        return (dx <= lx + EPSILON) and (dy <= ly + EPSILON) and (dz <= lz+ EPSILON)
+    elif NEIGHBORHOOD_TYPE == VON_NEUMANN:
+        if abs(dx - lx) < EPSILON:
+            return dy < ly - EPSILON
+        if abs(dy - ly) < EPSILON:
+            return dx < lx - EPSILON
+        return False
+    else: return False
+
